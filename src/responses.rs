c37593dc5cb261a6ef330d5efe091be6007a7462
Alex Krantz <alex@krantz.dev>
2024-01-24 14:55:06 -0800

    Remove unused error macro

--- src/responses.rs
+++ src/responses.rs
@@ -1,27 +1,6 @@
 use apollo_router::{graphql, services::supergraph};
 use http::StatusCode;
 use tower::BoxError;
-#[macro_export]
-macro_rules! error_response {
-    ($message:expr, req: $req:expr, service: $service:ident) => {
-        error_response!($message, "INTERNAL_ERROR", req: $req, service: $service)
-    };
-    ($message:expr, $code:expr, req: $req:expr, service: $service:ident) => {
-        error_response!($message, $code, http::StatusCode::INTERNAL_SERVER_ERROR, req: $req, service: $service)
-    };
-    ($message:expr, $code:expr, $status:expr, req: $req:expr, service: $service:ident) => {{
-        apollo_router::services::$service::Response::error_builder()
-            .error(
-                apollo_router::graphql::Error::builder()
-                    .message($message)
-                    .extension_code($code)
-                    .build(),
-            )
-            .status_code($status)
-            .context($req.context)
-            .build()?
-    }};
-}
 
 /// Create a response for an invalid request
 pub fn invalid<S: Into<String>>(
