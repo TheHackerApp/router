51afc19115e13584e9399fe6e070845070c09ccf
Alex Krantz <alex@krantz.dev>
2024-05-04 00:12:48 -0700

    Integrate with Hive GraphQL

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ mod http;
 mod plugins;
 mod responses;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
+use graphql_hive_router::{registry::HiveRegistry, usage};
 
 fn main() -> Result<()> {
+    usage::register();
+
+    HiveRegistry::new(None).context("failed to load GraphQL Hive registry")?;
+
     apollo_router::main()
 }
